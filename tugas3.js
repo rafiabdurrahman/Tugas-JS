f32f0ba979a471314765e6c740e53a91f5a4301e
Muhammad Rafi A <96464002+rafiabdurrahman@users.noreply.github.com>
2022-01-18 23:45:29 -0800

    Create tugas3.js

---
+++ tugas3.js
@@ -0,0 +1,23 @@
+//Ganjil & Genap
+function bilangan(nomor) {
+    if (nomor % 2 == 0) {
+        console.log('Adalah Genap')
+    } else {
+        console.log('Adalah Ganjil')
+    }
+}
+bilangan(357128)
+
+// Rumus persegi
+function persegi(sisi){
+    console.log(sisi * sisi)
+}
+
+persegi(9,9)
+
+// Rumus Presegi Panjang
+function persegipanjang(panjang,lebar){
+    console.log(panjang * lebar)
+}
+
+persegipanjang(26,12)
