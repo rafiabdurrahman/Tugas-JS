e06652b24ea97039f436d1e851866eb3f3e21e92
priagungjembar <priagungjembar1909@gmail.com>
2022-01-18 22:07:26 -0800

    first commit

---
+++ tes.js
@@ -0,0 +1,24 @@
+function perkenalan(){
+    console.log("Hi namaku adalah rafi");
+}
+perkenalan()
+
+function convertyear(year){
+    console.log(year * 365, "hari");
+}
+convertyear(1)
+
+function convertday(day){
+    console.log(day * 24, "jam");
+}
+convertday(1)
+
+function converthour(hour){
+    console.log(hour * 60, "menit");
+}
+converthour(1)
+
+function convertmin(min){
+    console.log(min * 60, "detik");
+}
+convertmin(1)
