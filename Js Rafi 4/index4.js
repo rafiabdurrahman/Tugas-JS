3e88bf773341758f1c0d734f66854d551fd96450
Muhammad Rafi A <96464002+rafiabdurrahman@users.noreply.github.com>
2022-01-26 22:52:05 -0800

    Add files via upload

---
+++ Js Rafi 4/index4.js
@@ -0,0 +1,37 @@
+// menghitung value unik
+
+const countUniqueValues = (Array) => {
+    const resultarray = []
+    for(const item of Array) {
+        if(resultarray.includes(item)) resultarray.push(item)
+    }
+    console.log(resultarray.length)
+
+}
+
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13,])
+countUniqueValues([1, 2, 2, 2, 2, 2, 2, 2, 4, 6])
+countUniqueValues([])
+
+
+//menyambung aray
+
+function nama(arr) {
+    console.log(arr.join(" "))
+}
+nama(['kafi','khoerul'])
+
+// boolean
+function boolean ( nomor, pembanding ) {
+    if ( nomor <= pembanding ) {
+        console.log("True");
+    }
+else{
+    console.log("False");
+}
+
+}
+boolean (10, 0)
+boolean (-1, 0)
+boolean (0)
+
