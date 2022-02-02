5030369eb9d0b35cba9f295781f2bb1a0cda1186
Muhammad Rafi A <96464002+rafiabdurrahman@users.noreply.github.com>
2022-02-01 22:25:13 -0800

    Update index.js

--- Js Rafi 4/Js Rafi 4/concat/index.js
+++ Js Rafi 4/Js Rafi 4/concat/index.js
@@ -1,5 +1,5 @@
-const arr1 = ["Rama", "Fatih"];
-const arr2 = ["Raihan", "Fauzy", "Fahmi"];
+const arr1 = ["Rafi", "Rafi"];
+const arr2 = ["Rafi", "Rafi", "Rafi"];
 const arr3 = ["Adjie", "Alvisyah"];
 const myFriend = arr1.concat(arr2, arr3);
 
